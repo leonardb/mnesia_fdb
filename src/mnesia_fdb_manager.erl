ff3fa24f1aeea3cfa39d283d6ab21b329169defb
leonardb <leonardb@users.noreply.github.com>
2020-06-04 11:15:45 -0400

    Update mnesia_fdb_manager.erl

--- src/mnesia_fdb_manager.erl
+++ src/mnesia_fdb_manager.erl
@@ -15,6 +15,8 @@
 %% specific language governing permissions and limitations
 %% under the License.
 %%----------------------------------------------------------------
+%% Support of FoundationDB:
+%%  Copyright 2020 Leonard Boyce <leonard.boyce@lucidlayer.com>
 
 -module(mnesia_fdb_manager).
 
